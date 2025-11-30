0be929f047ddffbcb1a30fc17031b788439ba81b
VacantArkade <kadehuskey@gmail.com>
2025-11-29 23:40:26 -0600

    Update

--- Assets/unity-chan!/Kade/UnityChanBoss.cs
+++ Assets/unity-chan!/Kade/UnityChanBoss.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
 
-public class UnityChanBoss : MonoBehaviour
+namespace Kade
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Temp use of enum to speed testing
+    public enum ChanStates
     {
-        
+        idle, pursue, melee, ranged
     }
-
-    // Update is called once per frame
-    void Update()
+    public class UnityChanBoss : MonoBehaviour
     {
-        
+        [Header("Chan Stats")]
+        [SerializeField] int maxHealth = 100;
+        private int currentHealth;
+        [SerializeField] float speed;
+
+        [Header("Chan Damage")]
+        [SerializeField] int basicAtkDamage;
+
+        [Header("Chan Controls")]
+        [SerializeField] GameObject sword;
+        [SerializeField] NavMeshAgent agent;
+        NavMeshPath path;
+        bool inMeleeRange = false;
+        Transform _transform;
+
+        Transform player;
+        void Start()
+        {
+            player = FindAnyObjectByType<PlayerLogic>().transform;
+            _transform = transform;
+        }
+
+        private void Awake()
+        {
+            currentHealth = maxHealth;
+        }
+
+        void Update()
+        {
+            agent.SetDestination(player.transform.position);
+        }
     }
 }
